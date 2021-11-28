1a6c8c255237ccb01caf799211095980eee22ca7
Klemen Zupan <zupanklem@gmail.com>
2021-11-28 13:40:36 +0100

    Finished section 4

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ class App extends React.Component {
     user: {},
     loading: false,
     alert: null,
+    repos: [],
   };
 
   static propTypes = {
@@ -38,6 +39,15 @@ class App extends React.Component {
     this.setState({ user: res.data, loading: false });
   };
 
+  // Get users repos
+  getUserRepos = async (username) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc`
+    );
+    this.setState({ repos: res.data, loading: false });
+  };
+
   // Clear users from state
   clearUsers = () => this.setState({ users: [], loading: false });
 
@@ -48,7 +58,7 @@ class App extends React.Component {
   };
 
   render() {
-    const { users, user, loading } = this.state;
+    const { users, user, repos, loading } = this.state;
     return (
       <Router>
         <div className="App">
@@ -79,7 +89,9 @@ class App extends React.Component {
                   <User
                     {...props}
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                     user={user}
+                    repos={repos}
                     loading={loading}
                   />
                 )}
